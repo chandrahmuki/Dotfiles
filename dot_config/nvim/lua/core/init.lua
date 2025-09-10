2aa05f25e71bf15f2be732732f56f2b94406ffc9
jerome Amouyal <amouyaljerome@gmail.com>
2025-09-10 16:07:31 +0000

    reset: replace old config with MuggyVim only

--- dot_config/nvim/lua/core/init.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/core/init.lua
-require("core.options")
-
-vim.g.mapleader = " "
-
--- basic keymaps
-vim.keymap.set("n", "<leader>q", ":q<CR>", { desc = "Quit" })
-vim.keymap.set("n", "<leader>w", ":w<CR>", { desc = "Save" })
-
--- dot_config/nvim/lua/core/options.lua
+++
@@ -1,8 +0,0 @@
--- ~/.config/nvim/lua/core/options.lua
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.termguicolors = true
-vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
-
